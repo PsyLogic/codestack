7347fc6afc9df64fe126318e291c694f85750890
psylogic <nemli.othmane@gmail.com>
2019-08-09 21:45:48 +0100

    Generate Model&migration file of Question

---
+++ database/migrations/2019_08_09_202848_create_questions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->unsignedInteger('views');
+            $table->unsignedInteger('answers');
+            $table->mediumInteger('votes')->default(0);
+            $table->unsignedSmallInteger('best_answer_id')->default(0);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
