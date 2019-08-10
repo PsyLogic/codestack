30eb0229b61e38c2c57e65618748187b01651cf1
psylogic <nemli.othmane@gmail.com>
2019-08-10 19:59:41 +0100

    Display All Questions and customize pagination

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        'title' => rtrim($faker->sentence()),
+        'title' => rtrim($faker->sentence(rand(2,3), ".")),
         'body' => $faker->paragraph(rand(1,5)),
         'views' => rand(0,100),
         'answers' => rand(0,15),
