e9964579bd340a515dd328a4c5bb6dc33de0047c
psylogic <nemli.othmane@gmail.com>
2019-08-11 19:05:06 +0100

    add answers,votes,views for each question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -8,6 +8,17 @@
                 <div class="card-body">
                     @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="votes">
+                                <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
+                                </div>
+                                <div class="answers {{ $question->status }} mb-1">
+                                    <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
+                                </div>
+                                <div class="views">
+                                    {{ $question->views }} {{ str_plural('view', $question->views) }}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <p class="lead">
