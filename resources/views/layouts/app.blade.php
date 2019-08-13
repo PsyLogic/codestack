7a6c78a831685da93cddf917c8d1b7a44b8823c8
psylogic <nemli.othmane@gmail.com>
2019-08-13 12:33:44 +0100

    add vote controls to question&answers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <script src="https://kit.fontawesome.com/fd32fda45b.js"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -4,21 +4,39 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">
-                    <h4>{{$question->title}}</h4>
-                </div>
                 <div class="card-body">
-                    {!!$question->body_html!!}
-                    <div class="float-right">
-                        <span class="text-muted">Asked {{ $question->create_date }}</span>
-                        <div class="media">
-                            <a href="{{ $question->user->url }}" class="pr-2">
-                                <img src="{{ $question->user->avatar }}" alt="{{ $question->user->name }}">
+                    <div class="card-title">
+                        <h4>{{$question->title}}</h4>
+                    </div>
+                    <hr>
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a href="" class="vote-up off" title="This question shows research effort; it is useful and clear">
+                                <i class="fas fa-caret-up fa-2x"></i>
                             </a>
-                            <div class="media-body mt-1">
-                                <a href="{{ $question->user->url }}" class="pr-2">
-                                    {{ $question->user->name }}
-                                </a>
+                            <span class="votes-count">344</span>
+                            <a href="" class="vote-down off" title="This question does not show any research effort; it is unclear or not useful">
+                                <i class="fas fa-caret-down fa-2x"></i>
+                            </a>
+                            <a href="" class="favorite mt-2 favorited" title="mark as favorite question (click again to undo)">
+                                <i class="fas fa-star" style="font-size: 1.3em;"></i>
+                                <span class="favorites-count">33</span>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            {!!$question->body_html!!}
+                            <div class="float-right">
+                                <span class="text-muted">Asked {{ $question->create_date }}</span>
+                                <div class="media">
+                                    <a href="{{ $question->user->url }}" class="pr-2">
+                                        <img src="{{ $question->user->avatar }}" alt="{{ $question->user->name }}">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{ $question->user->url }}" class="pr-2">
+                                            {{ $question->user->name }}
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -36,6 +54,19 @@
                     <hr>
                     @foreach ($question->answers as $answer)
                         <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a href="" class="vote-up off" title="This answer is useful">
+                                    <i class="fas fa-caret-up fa-2x"></i>
+                                </a>
+                                <span class="votes-count">344</span>
+                                <a href="" class="vote-down off" title="This answer is not useful">
+                                    <i class="fas fa-caret-down fa-2x"></i>
+                                </a>
+                                <a href="" class="best-answer mt-2 " title="The question owner accepted this as the best answer">
+                                    <i class="fas fa-check fa-3x"></i>
+                                </a>
+                                
+                            </div>
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
